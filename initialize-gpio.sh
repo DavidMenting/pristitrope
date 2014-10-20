8907aa2a4e7a786c2714900d7762d79e39c4a089
David Menting <david@davidmenting.nl>
2014-10-20 10:29:51 +0200

    Valid JSON config file & new gpio initialize script

--- initialize-gpio.sh
+++ initialize-gpio.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+PROG=/usr/local/bin/gpio
+
 ENCODE_UP=2
 ENCODE_DOWN=3
 TFT_ALL=18
@@ -6,12 +8,12 @@ TFT_NEXT=27
 TFT_UPDOWN=22
 FB_CS=23
 
-gpio -g mode $ENCODE_UP input
-gpio -g mode $ENCODE_DOWN input
-gpio -g mode $FB_CS input
+${PROG} -g mode $ENCODE_UP input
+${PROG} -g mode $ENCODE_DOWN input
+${PROG} -g mode $FB_CS input
 
-gpio -g mode $TFT_ALL output
-gpio -g mode $TFT_NEXT output
-gpio -g mode $TFT_UPDOWN output
+${PROG} -g mode $TFT_ALL output
+${PROG} -g mode $TFT_NEXT output
+${PROG} -g mode $TFT_UPDOWN output
 
-gpio write $ADDRESS_ALL 1
+${PROG} write $TFT_ALL 1
